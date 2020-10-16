9693d76425b919759e8a6eeb6492f2dc53a39034
ksenimini@yandex.ru <ksenimini@yandex.ru>
2020-10-16 22:35:36 +0700

    Square

--- addressbook-web-tests/addressbook-web-tests/UnitTest1.cs
+++ addressbook-web-tests/addressbook-web-tests/UnitTest1.cs
@@ -9,6 +9,17 @@ namespace addressbook_web_tests
         [TestMethod]
         public void TestMethod1()
         {
+            Square s1 = new Square(5);
+            Square s2 = new Square (10);
+            Square s3 = s1;
+
+            Assert.AreEqual(s1.Size, 5);
+            Assert.AreEqual(s2.Size, 10);
+            Assert.AreEqual(s3.Size, 5);
+
+            s3.Size = 15;
+            Assert.AreEqual(s1.Size, 15);
+
         }
     }
 }
